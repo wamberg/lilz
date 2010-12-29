1197d9b064e980f5fb1dd2e0a7aaee1e8eb6e2ca
William Amberg <wamberg@technushi.com>
2010-12-29 18:37:04 -0500

    Add dependency to local tetris.jar.  Add dependency locally by running 'mvn install:install-file -DgroupId=net.percederberg -DartifactId=tetris -Dversion=1.2.0 -Dpackaging=jar -Dfile=/path/to/tetris.jar'.  I used the java tetris implementation from http://www.percederberg.net/software/tetris/index.html.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject lilz "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [net.percederberg/tetris "1.2.0"]])
