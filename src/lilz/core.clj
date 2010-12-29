976ed8c90a7be4c9bcf8f3d1bc8078653c59a6f9
William Amberg <wamberg@technushi.com>
2010-12-29 18:10:42 -0500

    Bare Leiningen project.

---
+++ src/lilz/core.clj
@@ -0,0 +1,1 @@
+(ns lilz.core)
---
+++ test/lilz/test/core.clj
@@ -0,0 +1,6 @@
+(ns lilz.test.core
+  (:use [lilz.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
