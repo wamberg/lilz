ebae33418144a49007e530ffdf13934f5fa6150a
William Amberg <wamberg@technushi.com>
2010-12-30 16:47:01 -0500

    Turn on reflection warnings and remove current offenses.  Added (commented) code for accessing private methods through reflection.

--- src/lilz/bot.clj
+++ src/lilz/bot.clj
@@ -6,37 +6,37 @@
 
 (def robot (new java.awt.Robot))
 
-(defn init-game [bot]
+(defn init-game [^java.awt.Robot bot]
   (doto bot
     (.mouseMove 267 273) ; TODO: error prone for other screen resolutions
     (.mousePress java.awt.event.InputEvent/BUTTON1_MASK)
     (.mouseRelease java.awt.event.InputEvent/BUTTON1_MASK))
   (log/info "Game initialized"))
 
-(defn move-left [bot]
+(defn move-left [^java.awt.Robot bot]
   (.keyPress bot java.awt.event.KeyEvent/VK_LEFT)
   (.keyRelease bot java.awt.event.KeyEvent/VK_LEFT)
-  (log/info "Key Press: LEFT"))
-(defn move-right [bot]
+  (log/debug "Key Press: LEFT"))
+(defn move-right [^java.awt.Robot bot]
   (.keyPress bot java.awt.event.KeyEvent/VK_RIGHT)
   (.keyRelease bot java.awt.event.KeyEvent/VK_RIGHT)
-  (log/info "Key Press: RIGHT"))
-(defn move-down [bot]
+  (log/debug "Key Press: RIGHT"))
+(defn move-down [^java.awt.Robot bot]
   (.keyPress bot java.awt.event.KeyEvent/VK_DOWN)
   (.keyRelease bot java.awt.event.KeyEvent/VK_DOWN)
-  (log/info "Key Press: DOWN"))
-(defn rotate-clockwise [bot]
+  (log/debug "Key Press: DOWN"))
+(defn rotate-clockwise [^java.awt.Robot bot]
   (.keyPress bot java.awt.event.KeyEvent/VK_SHIFT)
   (.keyPress bot java.awt.event.KeyEvent/VK_UP)
   (.keyRelease bot java.awt.event.KeyEvent/VK_UP)
   (.keyRelease bot java.awt.event.KeyEvent/VK_SHIFT)
-  (log/info "Key Press: Clockwise"))
-(defn rotate-counter-clockwise [bot]
+  (log/debug "Key Press: Clockwise"))
+(defn rotate-counter-clockwise [^java.awt.Robot bot]
   (.keyPress bot java.awt.event.KeyEvent/VK_UP)
   (.keyRelease bot java.awt.event.KeyEvent/VK_UP)
-  (log/info "Key Press: Counter-Clockwise"))
+  (log/debug "Key Press: Counter-Clockwise"))
 
-(defn play-loop [bot]
+(defn play-loop [^java.awt.Robot bot]
   (while true
     ; test our agent
     (doseq [i (range 0 (rand-nth (range 0 5)))]
--- src/lilz/core.clj
+++ src/lilz/core.clj
@@ -1,6 +1,7 @@
+(set! *warn-on-reflection* true)
 (ns lilz.core
   (:gen-class)
-  (:require [lilz.bot]))
+  (:require [lilz.bot] [clojure.contrib.logging :as log]))
 (defn -main [& args]
   (import '(net.percederberg.tetris Game)
           '(java.awt Frame)
@@ -8,13 +9,18 @@
   (def frame (new java.awt.Frame "Tetris"))
   (def game (new net.percederberg.tetris.Game))
 
-  (doto frame
-    (.add (.getComponent game))
+  (doto ^java.awt.Frame frame
+    (.add (.getComponent ^net.percederberg.tetris.Game game))
     (.pack)
     (.addWindowListener (proxy [java.awt.event.WindowAdapter] []
                           (windowClosing [_] (System/exit 0))))
     (.show)
   )
+  ; java reflection to allow us to start the game
+;  (def handle-start (.getDeclaredMethod net.percederberg.tetris.Game "handleStart" nil))
+;  (.setAccessible handle-start true)
+;  (.invoke handle-start game nil)
+
   (lilz.bot/init-game lilz.bot/robot)
   (lilz.bot/play-loop lilz.bot/robot)
 )
